66c2b1c1e7b2e241b6b90f06012ced7fd7efc719
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2025-09-04 15:28:09 -0300

    start project

---
+++ StudioGhibli-2.0/StudioGhibli-2.0/StudioGhibli_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  StudioGhibli_2_0App.swift
+//  StudioGhibli-2.0
+//
+//  Created by Bianca on 04/09/25.
+//
+
+import SwiftUI
+
+@main
+struct StudioGhibli_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
